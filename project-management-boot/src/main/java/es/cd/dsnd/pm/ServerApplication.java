b9002b1f3e9602418f132b6143d4e4b576bcde85
Carlos Araya <carlosarayadelgado.job@gmail.com>
2021-11-15 11:07:35 +0100

    initial project

---
+++ project-management-boot/src/main/java/es/cd/dsnd/pm/ServerApplication.java
@@ -0,0 +1,16 @@
+package es.cd.dsnd.pm;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
