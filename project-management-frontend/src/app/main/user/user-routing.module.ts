516de792a3831627f6fc5b1a51ec87ecec7c8c4f
Ivan Arellano <daniel.i.arellano@hotmail.com>
2021-12-13 13:50:11 +0100

    users clickables

--- project-management-frontend/src/app/main/user/user-routing.module.ts
+++ project-management-frontend/src/app/main/user/user-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserHomeComponent } from './user-home/user-home.component';
-
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 const routes: Routes = [{
   path : '',
   component: UserHomeComponent
+},
+{
+  path:':user_',
+  component: UserDetailComponent
 }];
 
 @NgModule({
--- project-management-frontend/src/app/main/user/user.module.ts
+++ project-management-frontend/src/app/main/user/user.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { UserRoutingModule } from './user-routing.module';
 import { UserHomeComponent } from './user-home/user-home.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [UserHomeComponent],
+  declarations: [
+    UserHomeComponent,
+    UserDetailComponent],
   imports: [
     CommonModule,
     OntimizeWebModule,
+   SharedModule,
     UserRoutingModule
   ]
 })
