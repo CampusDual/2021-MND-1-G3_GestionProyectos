991570845334660e8fa62bdc3d2df378534eb081
yeralgarcia <yat1416@hotmail.com>
2021-12-14 13:34:52 +0100

    añadir project component detail

--- project-management-frontend/src/app/main/project/project-routing.module.ts
+++ project-management-frontend/src/app/main/project/project-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:':project_id',
+  component: ProjectDetailComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- project-management-frontend/src/app/main/project/project.module.ts
+++ project-management-frontend/src/app/main/project/project.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProjectRoutingModule } from './project-routing.module';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { OntimizeWebModule } from 'ontimize-web-ngx';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ ProjectDetailComponent],
   imports: [
     CommonModule,
+    OntimizeWebModule,
+    SharedModule,
     ProjectRoutingModule
   ]
 })
