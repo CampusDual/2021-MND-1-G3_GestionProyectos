7b017ed3486ac5c97067f4bc009aed250bcf9b0f
Ivan Arellano <daniel.i.arellano@hotmail.com>
2022-01-17 10:07:29 +0100

    boton delete funcionando

--- project-management-model/src/main/java/es/cd/dsnd/pm/model/core/service/UserService.java
+++ project-management-model/src/main/java/es/cd/dsnd/pm/model/core/service/UserService.java
@@ -45,8 +45,6 @@ public class UserService implements IUserService {
 	}
 
 	public EntityResult userDelete(Map<?, ?> keyMap) {
-		Map<Object, Object> attrMap = new HashMap<>();
-		attrMap.put("user_down_date", new Timestamp(Calendar.getInstance().getTimeInMillis()));
 		return this.daoHelper.update(this.userDao, attrMap, keyMap);
 	}
 
