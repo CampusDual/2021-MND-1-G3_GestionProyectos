85d85ec39c8966b55dd9c523068cde859bde0d47
yeralgarcia <yat1416@hotmail.com>
2022-01-17 13:52:01 +0100

    modificado ProjectService

--- project-management-model/src/main/java/es/cd/dsnd/pm/model/core/service/ProjectService.java
+++ project-management-model/src/main/java/es/cd/dsnd/pm/model/core/service/ProjectService.java
@@ -14,6 +14,8 @@ import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 
 import es.cd.dsnd.pm.api.core.service.IProjectService;
+import es.cd.dsnd.pm.model.core.dao.AreaDao;
+import es.cd.dsnd.pm.model.core.dao.AreaProjectDao;
 import es.cd.dsnd.pm.model.core.dao.ProjectDao;
 
 
@@ -24,6 +26,10 @@ public class ProjectService implements IProjectService {
 	@Autowired
 	private ProjectDao projectDao;
 
+	 @Autowired private AreaDao areaDao;
+
+	 @Autowired private AreaProjectDao areaProjectDao;
+	
 	@Autowired
 	private DefaultOntimizeDaoHelper daoHelper;
 
@@ -49,5 +55,49 @@ public class ProjectService implements IProjectService {
 	{
 		return this.daoHelper.delete(this.projectDao, keyMap);
 	}
+@Override
+public EntityResult areaQuery(Map<String, Object> keysValues, List<String> attributes)
+		throws OntimizeJEERuntimeException {
+
+	return this.daoHelper.query(this.areaDao, keysValues, attributes);
+}
+@Override
+public EntityResult areaInsert(Map<String, Object> attributes) throws OntimizeJEERuntimeException {
+
+	return this.daoHelper.insert(this.areaDao, attributes);
+}
+@Override
+public EntityResult areaUpdate(Map<String, Object> attributes, Map<String, Object> keyValues)
+		throws OntimizeJEERuntimeException {
+
+	return this.daoHelper.update(this.areaDao, keyValues, attributes);
+}
+@Override
+public EntityResult areaDelete(Map<String, Object> keyValues) throws OntimizeJEERuntimeException {
+
+	return this.daoHelper.delete(this.areaDao, keyValues);
+}
+@Override
+public EntityResult areaProjectQuery(Map<String, Object> keysValues, List<String> attributes)
+		throws OntimizeJEERuntimeException {
+
+	return this.daoHelper.query(this.areaProjectDao, keysValues, attributes);
+}
+@Override
+public EntityResult areaProjectInsert(Map<String, Object> attributes) throws OntimizeJEERuntimeException {
+
+	return this.daoHelper.insert(this.areaProjectDao, attributes);
+}
+@Override
+public EntityResult areaProjectUpdate(Map<String, Object> attributes, Map<String, Object> keyValues)
+		throws OntimizeJEERuntimeException {
+
+	return this.daoHelper.update(this.areaProjectDao, keyValues, attributes);
+}
+@Override
+public EntityResult areaProjectDelete(Map<String, Object> keyValues) throws OntimizeJEERuntimeException {
+
+	return this.daoHelper.delete(this.areaProjectDao, keyValues);
+}
 
 }
