1f71266acbaa239e98e32a878ef393d4843e9c74
Carlos Araya <carlosarayadelgado.job@gmail.com>
2022-01-18 14:21:59 +0100

    Intento new usuario

--- project-management-frontend/src/app/main/user/user-routing.module.ts
+++ project-management-frontend/src/app/main/user/user-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserHomeComponent } from './user-home/user-home.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserNewComponent } from './user-new/user-new.component';
 
 const routes: Routes = [{
   path : '',
   component: UserHomeComponent
+},
+{
+  path:':new',
+  component: UserNewComponent
 },
 {
   path:':user_',
--- project-management-frontend/src/app/main/user/user.module.ts
+++ project-management-frontend/src/app/main/user/user.module.ts
@@ -5,11 +5,13 @@ import { UserRoutingModule } from './user-routing.module';
 import { UserHomeComponent } from './user-home/user-home.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { SharedModule } from '../../shared/shared.module';
+import { UserNewComponent } from './user-new/user-new.component';
 
 @NgModule({
   declarations: [
     UserHomeComponent,
     UserDetailComponent,
+    UserNewComponent,
 ],
   imports: [
     CommonModule,
